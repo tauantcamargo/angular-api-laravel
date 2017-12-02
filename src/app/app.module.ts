5b3c508b9f35562264a05fbb2123168825f9479a
Allan Gonçalves da Cruz <allangcruz@gmail.com>
2017-12-02 07:00:52 -0200

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductsComponent } from './products/products.component';
 
+const appRoutes: Routes = [
+  { path : '', pathMatch: 'full', component : ProductListComponent },
+  { path : 'products', component : ProductListComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
