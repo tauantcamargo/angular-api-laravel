5472cf83b5bb6db28636af66745cb159740a73aa
Allan Gonçalves da Cruz <allangcruz@gmail.com>
2017-12-02 07:17:39 -0200

    listagem de produtos component

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() {
+    console.log('Bom dia allan');
+  }
+
+  ngOnInit() {
+  }
+
+}
